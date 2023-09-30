aa277aac89743c5058e9d73bafc7ee167308696d
Aung Pyay Thar <132377611+aungpyaythar1998@users.noreply.github.com>
2023-09-30 16:17:39 +0630

    Release the initial version of tic-tac-toe game

---
+++ script.js
@@ -0,0 +1,105 @@
+let gameBoard = (() => {
+    squares = [
+        "", "", "",
+        "", "", "",
+        "", "", "",
+    ];
+
+    const getAllSquares = () => squares;
+
+    return {
+        getAllSquares,
+    };
+})();
+
+
+let gameBoardUI = ((gameBoard) => {
+    let gameBoardSquares = gameBoard.getAllSquares();
+    let ticTacToeBoard = document.getElementById("game-board");
+    let squaresUI = ticTacToeBoard.children;
+
+    const renderEachSquare = (squareIndex, ticTacToeMark) => {
+        gameBoardSquares[squareIndex] = ticTacToeMark;
+        squaresUI[squareIndex].textContent = ticTacToeMark;
+    }
+
+    const render = () => {
+        let currentSquareIndex = 0;
+        for(let eachSquare of squaresUI) {
+            eachSquare.textContent = gameBoardSquares[currentSquareIndex++];
+        }
+    };
+
+    return {
+        renderEachSquare,
+        render,
+    };
+})(gameBoard);
+
+
+let Player = (name, mark) => {
+    let playerName = name;
+    let playerMark = mark;
+    let totalGamesPlayed = 0;
+    let gamesWon = 0;
+
+    const getName = () => playerName;
+    const getMark = () => playerMark;
+    const playGame = () => ++totalGamesPlayed;
+    const winGame = () => ++gamesWin;  
+    const getTotalGamesPlayed = () => totalGamesPlayed;
+    const getGamesWon = () => gamesWon;
+
+    return {
+        getName,
+        getMark,
+        getGamesWon,
+        getTotalGamesPlayed,
+        playGame,
+        winGame,
+    };
+};
+
+
+const players = [Player("Player 1", "O"), Player("Player 2", "X")];
+
+let currentPlayer = ((allPlayers) => {
+    let currentPlayerIndex = 0;
+    let player = allPlayers[currentPlayerIndex];
+
+    const getMark = () => player.getMark();
+    const changePlayer = () => {
+        if(currentPlayerIndex >= allPlayers.length - 1) {
+            currentPlayerIndex = 0;
+        } else {
+            currentPlayerIndex++;
+        }
+        player = allPlayers[currentPlayerIndex];
+    };
+
+    return {
+        getMark,
+        changePlayer,
+    }
+
+})(players);
+
+
+let gameBoardSquares = document.getElementsByClassName("game-board-square");
+Array.from(gameBoardSquares).forEach((eachSquare) => {
+    eachSquare.addEventListener("click", function () {
+        if(this.textContent != "") {
+            return;
+        }
+        let currentSquareIndex = Number(this.getAttribute("data-index"));
+        gameBoardUI.renderEachSquare(
+            currentSquareIndex,
+            currentPlayer.getMark(),
+        );
+        currentPlayer.changePlayer();
+    });
+})
+
+
+
+
