a739d0ebc411987ec011a88e68c7deacaf96dd70
Aung Pyay Thar <132377611+aungpyaythar1998@users.noreply.github.com>
2023-10-01 17:32:51 +0630

    Add checks for wins/draws
    Add functionality for checking win and draw results. Make
    gameBoard.showGameResult() accept message to be shown in the modal
    dialog as a parameter. Expose getName from currentPlayer object.
    Correct gameBoardUI.render to reflect on the changes made to
    gameBoard.squares

--- script.js
+++ script.js
@@ -5,30 +5,62 @@ let gameBoard = (() => {
         "", "", "",
     ];
     let emptySquares = squares.length;
+    let allWinningPositions = [
+        [3, 6, 4, 8, 1, 2], [0, 2, 4, 7], [1, 0, 4, 6, 5, 8],
+        [0, 6, 4, 5], [3, 5, 1, 7, 0, 8, 2, 6], [3, 4, 2, 8],
+        [0, 3, 4, 2, 7, 8], [6, 8, 4, 1], [6, 7, 0, 4, 2, 5],
+    ];
 
-    const showGameResult = () => {
+    const getAllSquares = () => squares;
+
+    const showGameResult = (message) => {
         let gameResultDialog = document.getElementById("game-result-popup");
+        let gameResultDisplay = document.getElementById("game-result-text");
+
+        gameResultDisplay.textContent = message;
         gameResultDialog.showModal();
     };
 
-    const getAllSquares = () => squares;
+    const checkIfWinIdx = (idx) => (squares[idx] === currentPlayer.getMark());
+
+    const checkGameResult = (idx, value) => {
+       let winningPositions = allWinningPositions[idx];
+       let i = 0;
+       while(i < winningPositions.length) {
+        if(checkIfWinIdx(winningPositions[i]) && checkIfWinIdx(winningPositions[i + 1])) {
+            showGameResult(`${currentPlayer.getName()} wins!`);
+            break;
+        } else if (emptySquares === 1) {
+            showGameResult("It's a draw!");
+        }
+        i += 2;
+       }
+    };
+
     const tickASquare = (idx, value) => {
         squares[idx] = value;
+        checkGameResult(idx, value);
         emptySquares--;
-        if (emptySquares === 0) {
-            showGameResult();
-        }
+    }
+
+    const resetGameBoard = () => {
+        squares = [
+            "", "", "",
+            "", "", "",
+            "", "", "",
+        ];
+        emptySquares = squares.length;
     }
 
     return {
         getAllSquares,
         tickASquare,
+        resetGameBoard,
     };
 })();
 
 
 let gameBoardUI = ((gameBoard) => {
-    let gameBoardSquares = gameBoard.getAllSquares();
     let ticTacToeBoard = document.getElementById("game-board");
     let squaresUI = ticTacToeBoard.children;
 
@@ -38,6 +70,7 @@ let gameBoardUI = ((gameBoard) => {
     };
 
     const render = () => {
+        let gameBoardSquares = gameBoard.getAllSquares();
         let currentSquareIndex = 0;
         for(let eachSquare of squaresUI) {
             eachSquare.textContent = gameBoardSquares[currentSquareIndex++];
@@ -81,6 +114,7 @@ let currentPlayer = ((allPlayers) => {
     let currentPlayerIndex = 0;
     let player = allPlayers[currentPlayerIndex];
 
+    const getName = () => player.getName();
     const getMark = () => player.getMark();
     const changePlayer = () => {
         if(currentPlayerIndex >= allPlayers.length - 1) {
@@ -92,6 +126,7 @@ let currentPlayer = ((allPlayers) => {
     };
 
     return {
+        getName,
         getMark,
         changePlayer,
     }
